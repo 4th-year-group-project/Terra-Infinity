00562dd02b4e9c40231f288e2d5f01388bc7582b
Ethan Farrell <Ethan.Farrell@warwick.ac.uk>
2024-12-03 01:16:23 +0000

    Modify mesh geneartion to work for new heightmap format

--- renderer/src/experimental/simple_mesh_generation.cpp
+++ renderer/src/experimental/simple_mesh_generation.cpp
@@ -19,14 +19,14 @@
     #include <glm/gtc/type_ptr.hpp>
 #endif
 
-double bilinear_interpolation(double x, double z, double **image, double x1, double x2, double z1, double z2) {
-    double r1 = (x2 - x) / (x2 - x1) * image[static_cast<int>(z1)][static_cast<int>(x)] + (x - x1) / (x2 - x1) * image[static_cast<int>(z1)][static_cast<int>(x2)];
-    double r2 = (x2 - x) / (x2 - x1) * image[static_cast<int>(z2)][static_cast<int>(x)] + (x - x1) / (x2 - x1) * image[static_cast<int>(z2)][static_cast<int>(x2)];
+float bilinear_interpolation(float x, float z, float **image, float x1, float x2, float z1, float z2) {
+    float r1 = (x2 - x) / (x2 - x1) * image[static_cast<int>(z1)][static_cast<int>(x)] + (x - x1) / (x2 - x1) * image[static_cast<int>(z1)][static_cast<int>(x2)];
+    float r2 = (x2 - x) / (x2 - x1) * image[static_cast<int>(z2)][static_cast<int>(x)] + (x - x1) / (x2 - x1) * image[static_cast<int>(z2)][static_cast<int>(x2)];
     return (z2 - z) / (z2 - z1) * r1 + (z - z1) / (z2 - z1) * r2;
     // Check if this can be optimized
 }
 
-double sgn(double x) {
+float sgn(float x) {
     if (x > 0) {
         return 1.0;
     } else if (x < 0) {
@@ -36,26 +36,24 @@ double sgn(double x) {
     }
 }
 
-
-
 // A function to read a heightmap from a RAW file and store it in a 2D array
 // The heightmap is assumed to be a square
-double **read_heightmap(const char *filename, int size) {
+float **read_heightmap(const char *filename, int size) {
     FILE *file;
     file = fopen(filename, "rb");
     if (file == NULL) {
         std::cout << "Error: Could not open file" << std::endl;
         return NULL;
     }
-    double **heightmap = new double*[size];
+    float **heightmap = new float*[size];
     for (int i = 0; i < size; i++) {
-        heightmap[i] = new double[size];
+        heightmap[i] = new float[size];
     }
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            // read the next double from the file
-            double height;
-            fread(&height, sizeof(double), 1, file);
+            // read the next float from the file
+            float height;
+            fread(&height, sizeof(float), 1, file);
             heightmap[i][j] = height;
         }
     }
@@ -63,7 +61,7 @@ double **read_heightmap(const char *filename, int size) {
     return heightmap;
 }
 
-double height_scaling(double value, int type, double scale_factor, double function_factor, double blend_factor=0.5) {
+float height_scaling(float value, int type, float scale_factor, float function_factor, float blend_factor=0.5) {
     switch (type)
     {
     case 0:
@@ -97,23 +95,22 @@ double height_scaling(double value, int type, double scale_factor, double functi
 // mesh is higher than the resolution of the heightmap
 // Size is the size of the heightmap and resolution is the resolution of the mesh
 
-
-std::vector<std::vector<glm::vec3>> generate_mesh(double **heightmap, int size, int resolution) {
+std::vector<std::vector<glm::vec3>> generate_mesh(float **heightmap, int size, int resolution) {
     // We want to scale the heightmap values from the range [0,1] to the range [0, 64]
-    double function_factor = 0.5;
-    double scaling_factor = 64.0;
+    float function_factor = 0.5;
+    float scaling_factor = 96.0;
     std::vector<std::vector<glm::vec3>> mesh = std::vector<std::vector<glm::vec3>>(resolution, std::vector<glm::vec3>(resolution));
 
-    double step = static_cast<double>(size) / static_cast<double>(resolution);
+    float step = static_cast<float>(size) / static_cast<float>(resolution);
 
     for (int i = 0; i < resolution; i++) {
         for (int j = 0; j < resolution; j++) {
-            double x = i * step;
-            double z = j * step;
-            double x1 = static_cast<int>(x);
-            double x2 = x1 + 1;
-            double z1 = static_cast<int>(z);
-            double z2 = z1 + 1;
+            float x = i * step;
+            float z = j * step;
+            float x1 = static_cast<int>(x);
+            float x2 = x1 + 1;
+            float z1 = static_cast<int>(z);
+            float z2 = z1 + 1;
             if ((x2 >= size || z2 >= size) || (x1 == x && z1 == z && x2 == x + 1 && z2 == z + 1)) {
                 mesh[j][i] = glm::vec3(x, height_scaling(heightmap[static_cast<int>(z)][static_cast<int>(x)], 0, scaling_factor, function_factor), z);
             } else {
@@ -124,12 +121,12 @@ std::vector<std::vector<glm::vec3>> generate_mesh(double **heightmap, int size, 
 
     // for (int i = 0; i < resolution; i++) {
     //     for (int j = 0; j < resolution; j++) {
-    //         double x = j * step;
-    //         double z = i * step;
-    //         double x1 = static_cast<int>(x);
-    //         double x2 = static_cast<int>(x) + 1;
-    //         double z1 = static_cast<int>(z);
-    //         double z2 = static_cast<int>(z) + 1;
+    //         float x = j * step;
+    //         float z = i * step;
+    //         float x1 = static_cast<int>(x);
+    //         float x2 = static_cast<int>(x) + 1;
+    //         float z1 = static_cast<int>(z);
+    //         float z2 = static_cast<int>(z) + 1;
     //         if ((x2 >= size || z2 >= size) || (x1 == x && z1 == z && x2 == x + 1 && z2 == z + 1)) {
     //             mesh[i][j] = glm::vec3(x, height_scaling(heightmap[static_cast<int>(z)][static_cast<int>(x)], 0, scaling_factor, function_factor), z);
     //         } else {
@@ -155,27 +152,22 @@ std::vector<glm::vec3> flatten_mesh(std::vector<std::vector<glm::vec3>> mesh, in
 // We need to generate the index buffer for OpenGL to render the mesh
 // Each quad will have 2 triangles and each triangle will have 3 vertices
 // The widing order of the triangles is important and is clockwise
-std::vector<glm::vec3> generate_index_buffer(int resolution) {
-    std::vector<glm::vec3> index_buffer = std::vector<glm::vec3>((resolution - 1) * (resolution - 1) * 2);
-
+std::vector<unsigned int> generate_index_buffer(int resolution) {
+    std::vector<unsigned int> index_buffer = std::vector<unsigned int>((resolution - 1) * (resolution - 1) * 2 * 3);
     for (int i = 0; i < resolution - 1; i++) {
         for (int j = 0; j < resolution - 1; j++) {
             // First triangle of the form [i,j], [i+1, j+1], [i+1, j]
-            index_buffer[i * (resolution - 1) * 2 + j * 2] = glm::vec3(i * resolution + j, (i + 1) * resolution + j + 1, (i + 1) * resolution + j);
+            index_buffer[i * (resolution - 1) * 6 + j * 6] = i * resolution + j;
+            index_buffer[i * (resolution - 1) * 6 + j * 6 + 1] = (i + 1) * resolution + j + 1;
+            index_buffer[i * (resolution - 1) * 6 + j * 6 + 2] = (i + 1) * resolution + j;
+
             // Second triangle of the form [i,j], [i, j+1], [i+1, j+1]
-            index_buffer[i * (resolution - 1) * 2 + j * 2 + 1] = glm::vec3(i * resolution + j, i * resolution + j + 1, (i + 1) * resolution + j + 1);
+            index_buffer[i * (resolution - 1) * 6 + j * 6 + 3] = i * resolution + j;
+            index_buffer[i * (resolution - 1) * 6 + j * 6 + 4] = i * resolution + j + 1;
+            index_buffer[i * (resolution - 1) * 6 + j * 6 + 5] = (i + 1) * resolution + j + 1;
         }
     }
     return index_buffer;
-    // for (int i = 0; i < resolution - 1; i++) {
-    //     for (int j = 0; j < resolution - 1; j++) {
-    //         // First triangle
-    //         index_buffer[i * (resolution - 1) * 2 + j * 2] = glm::vec3(i * resolution + j, i * resolution + j + 1, (i + 1) * resolution + j);
-    //         // Second triangle
-    //         index_buffer[i * (resolution - 1) * 2 + j * 2 + 1] = glm::vec3(i * resolution + j + 1, (i + 1) * resolution + j + 1, (i + 1) * resolution + j);
-    //     }
-    // }
-    // return index_buffer;
 }
 
 glm::vec3 compute_normal_contribution(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3) {
@@ -312,81 +304,6 @@ std::vector<std::vector<glm::vec3>> generate_vertices_normals(std::vector<std::v
                 glm::vec3 face6Norm = compute_normal_contribution(A, B, C);
                 vertices_normals[i][j] = glm::normalize(face1Norm + face2Norm + face3Norm + face4Norm + face5Norm + face6Norm);
             }
-
-
-            // // The corner cases of [0,0]
-            // if ((i == 0 && j == 0)) {
-            //     glm::vec3 up = mesh[i + 1][j] - mesh[i][j];
-            //     glm::vec3 upRight = mesh[i + 1][j + 1] - mesh[i][j];
-            //     glm::vec3 right = mesh[i][j + 1] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(upRight, up) + glm::cross(right, upRight);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The corner case of [0, resolution - 1]
-            // } else if (i == 0 && j == resolution - 1) {
-            //     glm::vec3 left = mesh[i][j - 1] - mesh[i][j];
-            //     glm::vec3 up = mesh[i + 1][j] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(up, left);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The corner case of [resolution - 1, 0]
-            // } else if (i == resolution - 1 && j == 0) {
-            //     glm::vec3 down = mesh[i - 1][j] - mesh[i][j];
-            //     glm::vec3 right = mesh[i][j + 1] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(down, right);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The corner case of [resolution - 1, resolution - 1]
-            // } else if (i == resolution - 1 && j == resolution - 1) {
-            //     glm::vec3 down = mesh[i - 1][j] - mesh[i][j];
-            //     glm::vec3 downLeft = mesh[i - 1][j - 1] - mesh[i][j];
-            //     glm::vec3 left = mesh[i][j - 1] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(downLeft, down) + glm::cross(left, downLeft);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The edge cases of the bottom row
-            // } else if (i == 0) {
-            //     glm::vec3 left = mesh[i][j - 1] - mesh[i][j];
-            //     glm::vec3 up = mesh[i + 1][j] - mesh[i][j];
-            //     glm::vec3 upRight = mesh[i + 1][j + 1] - mesh[i][j];
-            //     glm::vec3 right = mesh[i][j + 1] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(up, left) + glm::cross(upRight, up) + glm::cross(right, upRight);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The edge cases of the left row
-            // } else if (j == 0) {
-            //     glm::vec3 up = mesh[i + 1][j] - mesh[i][j];
-            //     glm::vec3 upRight = mesh[i + 1][j + 1] - mesh[i][j];
-            //     glm::vec3 right = mesh[i][j + 1] - mesh[i][j];
-            //     glm::vec3 down = mesh[i - 1][j] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(upRight, up) + glm::cross(right, upRight) + glm::cross(down, right);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The edge cases of the top row
-            // } else if (i == resolution - 1) {
-            //     glm::vec3 right = mesh[i][j + 1] - mesh[i][j];
-            //     glm::vec3 down = mesh[i - 1][j] - mesh[i][j];
-            //     glm::vec3 downLeft = mesh[i - 1][j - 1] - mesh[i][j];
-            //     glm::vec3 left = mesh[i][j - 1] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(down, right) + glm::cross(downLeft, down) + glm::cross(left, downLeft);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The edge cases of the right row
-            // } else if (j == resolution - 1) {
-            //     glm::vec3 down = mesh[i - 1][j] - mesh[i][j];
-            //     glm::vec3 downLeft = mesh[i - 1][j - 1] - mesh[i][j];
-            //     glm::vec3 left = mesh[i][j - 1] - mesh[i][j];
-            //     glm::vec3 up = mesh[i + 1][j] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(downLeft, down) + glm::cross(left, downLeft) + glm::cross(up, left);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // // The general case
-            // } else {
-            //     glm::vec3 up = mesh[i + 1][j] - mesh[i][j];
-            //     glm::vec3 upRight = mesh[i + 1][j + 1] - mesh[i][j];
-            //     glm::vec3 right = mesh[i][j + 1] - mesh[i][j];
-            //     glm::vec3 down = mesh[i - 1][j] - mesh[i][j];
-            //     glm::vec3 downLeft = mesh[i - 1][j - 1] - mesh[i][j];
-            //     glm::vec3 left = mesh[i][j - 1] - mesh[i][j];
-            //     glm::vec3 sum = glm::cross(up, left) + glm::cross(upRight, up) + glm::cross(right, upRight) + glm::cross(down, right) + glm::cross(downLeft, down) + glm::cross(left, downLeft);
-            //     vertices_normals[i][j] = glm::normalize(sum);
-            // }
-            // if (vertices_normals[i][j][1] > 0) {
-            //     std::cout << "Positive normal: " << vertices_normals [i][j][0] << " " << vertices_normals[i][j][1] << " " << vertices_normals[i][j][2] << std::endl;
-            //     std::cout << "i: " << i << " j: " << j << std::endl;
-            // }
         }
     }
     return vertices_normals;
@@ -402,8 +319,7 @@ std::vector<glm::vec3> flatten_normals(std::vector<std::vector<glm::vec3>> norma
     return flattened_normals;
 }
 
-// TODO: Implement a function to save the rendered mesh to an obj file
-void storeToObj(std::vector<glm::vec3> vertices, std::vector<glm::vec3> indices, std::vector<glm::vec3> normals, int resolution, int indexBufferSize, std::string path) {
+void storeToObj(std::vector<glm::vec3> vertices, std::vector<unsigned> indices, std::vector<glm::vec3> normals, int resolution, int indexBufferSize, std::string path) {
     std::ofstream objFile;
     objFile.open(path);
     objFile << "# OBJ file" << std::endl;
@@ -418,8 +334,9 @@ void storeToObj(std::vector<glm::vec3> vertices, std::vector<glm::vec3> indices,
     // The indices are 0-indexed and need to be converted to 1-indexed
     // The vertex entries will pair up with the corresponding vertex normal entries
     objFile << "# Faces (vertex // vertex normal)" << std::endl;
-    for (int i = 0; i < indexBufferSize; i++) {
-        objFile << "f " << indices[i][0] + 1 << "//" << indices[i][0] + 1 << " " << indices[i][1] + 1 << "//" << indices[i][1] + 1 << " " << indices[i][2] + 1 << "//" << indices[i][2] + 1 << std::endl;
+    for (int i = 0; i < indexBufferSize * 3; i += 3) {
+        // We need to ensure that the indices are not written in scientific notation
+        objFile << "f " << indices[i] + 1 << "//" << indices[i] + 1 << " " << indices[i + 1] + 1 << "//" << indices[i + 1] + 1 << " " << indices[i + 2] + 1 << "//" << indices[i + 2] + 1 << std::endl;
     }
     objFile.close();
 }
@@ -430,9 +347,10 @@ int main(void) {
     // Read the environment variable $PROJECT_ROOT
     std::string project_root = std::getenv("PROJECT_ROOT");
     // Read the heightmap
-    std::string filename = project_root + "/data/simplex_heightmap.raw";
-    int heightmapSize = 512;
-    double **heightmap = read_heightmap(filename.c_str(), heightmapSize);
+    std::string filename = project_root + "/data/noise_coast_map.raw";
+    int heightmapSize = 1024;
+    int meshResolution = 1024;
+    float **heightmap = read_heightmap(filename.c_str(), heightmapSize);
     // Print the heightmap
     // for (int i = 0; i < heightmapSize; i++) {
     //     for (int j = 0; j < heightmapSize; j++) {
@@ -446,7 +364,6 @@ int main(void) {
     // std::cout << heightmap[1][0] << std::endl;
 
     // Generate the mesh
-    int meshResolution = 512;
     std::vector<std::vector<glm::vec3>> mesh = generate_mesh(heightmap, heightmapSize, meshResolution);
     // Print the first few values
     // std::cout << "First few values: " << std::endl;
@@ -465,21 +382,21 @@ int main(void) {
     // std::cout << flattened_mesh[2][0] << " " << flattened_mesh[2][1] << " " << flattened_mesh[2][2] << std::endl;
     // std::cout << flattened_mesh[1024][0] << " " << flattened_mesh[1024][1] << " " << flattened_mesh[1024][2] << std::endl;
 
-    std::vector<glm::vec3> index_buffer = generate_index_buffer(meshResolution);
+    std::vector<unsigned int> index_buffer = generate_index_buffer(meshResolution);
     std::cout << "Index buffer size: " << index_buffer.size() << std::endl;
     // Print the first few values
     std::cout << "First few values: " << std::endl;
-    std::cout << index_buffer[0][0] << " " << index_buffer[0][1] << " " << index_buffer[0][2] << std::endl;
-    std::cout << index_buffer[1][0] << " " << index_buffer[1][1] << " " << index_buffer[1][2] << std::endl;
-    std::cout << index_buffer[2][0] << " " << index_buffer[2][1] << " " << index_buffer[2][2] << std::endl;
-    std::cout << index_buffer[3][0] << " " << index_buffer[3][1] << " " << index_buffer[3][2] << std::endl;
+    std::cout << index_buffer[0] << " " << index_buffer[1] << " " << index_buffer[2] << std::endl;
+    std::cout << index_buffer[3] << " " << index_buffer[4] << " " << index_buffer[5] << std::endl;
+    std::cout << index_buffer[6] << " " << index_buffer[7] << " " << index_buffer[8] << std::endl;
+    std::cout << index_buffer[9] << " " << index_buffer[10] << " " << index_buffer[11] << std::endl;
 
     std::vector<std::vector<glm::vec3>> vertices_normals = generate_vertices_normals(mesh, meshResolution);
     std::vector<glm::vec3> flattened_normals = flatten_normals(vertices_normals, meshResolution);
     std::cout << "Flattened normals size: " << flattened_normals.size() << std::endl;
 
     int indexBufferSize = (meshResolution - 1) * (meshResolution - 1) * 2;
-    std::string path = project_root + "/data/simplex_mesh_with_normals_5.obj";
+    std::string path = project_root + "/data/noise_coast_map1.obj";
     storeToObj(flattened_mesh, index_buffer, flattened_normals, meshResolution, indexBufferSize, path);
     return 0;
 }
