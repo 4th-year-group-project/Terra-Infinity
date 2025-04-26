34af18ff709da1734c779b73598ffdf9a969d267
Ethan Wakefield <ethanwakefield000@gmail.com>
2025-04-26 11:26:42 +0100

    docstrings

--- biomes/land_water_map.py
+++ biomes/land_water_map.py
@@ -2,13 +2,26 @@ from copy import deepcopy
 
 import numpy as np
 
-# from perlin_noise import PerlinNoise
 from generation import Noise
 
 
 def generate_landmass_heights(seed, centroids, scale=5000, sharpness=0):
+    """
+    Generate the simplex noise values for a set of points. Used with Voronoi
+    centroids to help determine landmass and ocean cells. 
+
+    Args:
+        seed: Random seed for noise generation.
+        centroids: Centroids of the Voronoi cells.
+        scale: Scale of the noise.
+        sharpness: Sharpness of the noise. 
+
+    Returns:
+        heights: A list of noise values for the centroids.
+    """
     centroids_np = np.array(centroids)
 
+    # Generate the simplex noise points
     noise = Noise(seed)
     heights = noise.batch_simplex_noise(
         centroids_np, octaves=3,
@@ -26,62 +39,48 @@ def generate_landmass_heights(seed, centroids, scale=5000, sharpness=0):
     return heights
 
 def determine_landmass(polygon_edges, polygon_points, shared_edges, polygon_ids, coords, seed, polygon_centers, parameters):
+    """Assign landmass and ocean cells to Voronoi polygons.
 
-    # high_thresh = 0.4
-    # low_thresh = -0.5
+    Polygons are assigned based on values of a global noise map, which is consistent across independent
+    calls. 
+
+    Args:
+        polygon_edges: List of edges for each Voronoi polygon, coordinates in global space.
+        polygon_points: List of points for each Voronoi polygon, coordinates in global space.
+        shared_edges: List of edges shared between Voronoi polygons.
+        polygon_ids: List of unique IDs for each Voronoi polygon.
+        coords: Chunk coordinates for the desired superchunk.
+        seed: Random seed for consistency of random processes.
+        polygon_centers: List of centroids for each Voronoi polygon.
+        parameters: Dictionary of parameters from the UI.
+
+    Returns:
+        polygon_edges: List of edges for each Voronoi polygon, coordinates in global space.
+        polygons_to_return: List of points for each Voronoi polygon, coordinates in local space.
+        land_water_ids: List of 0s or 1s for each Voronoi polygon, indicating water or land respectively.
+        slice_parts: Two points that define a square. This square tells you where the superchunk is in local space.
+        polygons_to_return_og_coord_space: List of points for each Voronoi polygon, coordinates in global space.
+        overall_min_x: Minimum x coordinate of the Voronoi polygons in global space.
+        overall_min_y: Minimum y coordinate of the Voronoi polygons in global space.
+    """
+
     ocean_coverage = parameters.get("ocean_coverage", 50)
-    # normalised_thresh =  (((ocean_coverage) / 100) * (high_thresh - low_thresh)) + low_thresh
-
-    # land_water_scale = parameters.get("land_water_scale", 50)
-    # land_water_scale_normalised = (((land_water_scale) / 100) * (1300 - 500)) + 500
-    # land_water_scale_normalised /= 10
-
     polygon_points_copy = deepcopy(polygon_points)
 
+    # Find the minimum and maximum x and y of the superchunk in global space
     start_coords_x = coords[0] * 1023
     end_coords_x = (coords[0] + 1) * 1023
     start_coords_y = coords[1] * 1023
     end_coords_y = (coords[1] + 1) * 1023
-
     all_points = np.vstack(polygon_points)
-
     overall_min_x, overall_min_y = np.round(np.min(all_points, axis=0)).astype(int)
-    overall_max_x, overall_max_y = np.round(np.max(all_points, axis=0)).astype(int)
 
-    # x_offset = overall_min_x/10
-    # y_offset = overall_min_y/10
-
-    # noise = Noise(seed=seed, width=int(abs(overall_min_x - overall_max_x)/10), height=int(abs(overall_min_y - overall_max_y)/10))
-
-    # t_noise_1 = noise.fractal_simplex_noise(noise="open",
-    #                                         x_offset=int(x_offset), y_offset=int(y_offset),
-    #                                         scale=land_water_scale_normalised, octaves=1, persistence=0.5, lacunarity=2.0)
-
-    # t_noise_2 = noise.fractal_simplex_noise(noise="open",
-    #                                         x_offset=int(x_offset), y_offset=int(y_offset),
-    #                                         scale=land_water_scale_normalised, octaves=3, persistence=0.5, lacunarity=2.0)
-    # t_noise = 0.4 * t_noise_1 + 0.6 * t_noise_2
-
-    # map = t_noise
-    # map = cv2.resize(map, (int(abs(overall_max_x - overall_min_x)), int(abs(overall_max_y - overall_min_y))), interpolation=cv2.INTER_LINEAR)
-
-    # # scale up the tempmap using interpolation
-    # # map = cv2.resize(map, (int(np.ceil(overall_max_x - overall_min_x)), int(np.ceil(overall_max_y - overall_min_y))), interpolation=cv2.INTER_LINEAR)
-
-    # threshold = normalised_thresh
-    # pic = np.array(map)
-
-    # pic[pic > threshold] = 1
-    # pic[pic <= threshold] = 0
-    # binary_image = pic
-
-    # fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-    # fig.tight_layout()
-
+    # Find the heights in the noise map which will define landmass and ocean
     relevant_polygon_ids = []
     water_polygon_ids = []
     landmass_heights = generate_landmass_heights(seed, polygon_centers)
 
+    # Threshold the heights to determine landmass and ocean
     for i in range(len(landmass_heights)):
         polygon_id = polygon_ids[i]
         if landmass_heights[i] < -0.1:
@@ -93,7 +92,7 @@ def determine_landmass(polygon_edges, polygon_points, shared_edges, polygon_ids,
         for i in range(len(polygon)):
             polygon[i] = (polygon[i][0] - overall_min_x, polygon[i][1] - overall_min_y)
 
-
+    # Find the minimum and maximum x and y of the superchunk in local space
     start_coords_x = start_coords_x - overall_min_x
     end_coords_x = end_coords_x - overall_min_x
     start_coords_y = start_coords_y - overall_min_y
@@ -101,11 +100,12 @@ def determine_landmass(polygon_edges, polygon_points, shared_edges, polygon_ids,
 
     slice_parts = (start_coords_x, end_coords_x, start_coords_y, end_coords_y)
 
-
+    # Construct required data structures
     polygons_to_return = []
     polygons_to_return_og_coord_space = []
     land_water_ids = []
 
+    # Iterate through the polygons and assign them as land or water
     for i in range(len(polygon_points)):
         polygons_to_return.append(polygon_points[i])
         polygons_to_return_og_coord_space.append(polygon_points_copy[i])
@@ -114,12 +114,22 @@ def determine_landmass(polygon_edges, polygon_points, shared_edges, polygon_ids,
         else:
             land_water_ids.append(0)
 
-
-
     return polygon_edges, polygons_to_return, land_water_ids, slice_parts, polygons_to_return_og_coord_space, (overall_min_x, overall_min_y)
 
 
 def find_polygon_centroid_value(polygon, x_min, y_min, binary_image):
+    """Finds the value of the centroid of a polygon in a corresponding black/white image.
+
+    Args:
+        polygon: List of points defining the polygon.
+        x_min: Minimum x coordinate of the polygon.
+        y_min: Minimum y coordinate of the polygon.
+        binary_image: Binary image to query with a position.
+
+    Returns:
+        The value of the centroid in the binary image.
+    """
+    # Adjust the polygon coordinates to be relative to the binary image
     for i in range(len(polygon)):
         polygon[i] = (polygon[i][0] - x_min, polygon[i][1] - y_min)
 
@@ -131,40 +141,6 @@ def find_polygon_centroid_value(polygon, x_min, y_min, binary_image):
     min_polygon_y = int(np.floor(min(y_points)))
     max_polygon_y = int(np.ceil(max(y_points)))
 
+    # Find centroid
     centroid = ((min_polygon_x + max_polygon_x) // 2, (min_polygon_y + max_polygon_y) // 2)
     return binary_image[centroid[1], centroid[0]]
-
-
-# def is_polygon_covering_image(polygon, x_min, y_min, binary_image, threshold=0.5):
-#     start_time = time.time()
-#     for i in range(len(polygon)):
-#         polygon[i] = (polygon[i][0] - x_min, polygon[i][1] - y_min)
-
-#     x_points = [point[0] for point in polygon]
-#     y_points = [point[1] for point in polygon]
-
-#     min_polygon_x = int(np.floor(min(x_points)))
-#     max_polygon_x = int(np.ceil(max(x_points)))
-#     min_polygon_y = int(np.floor(min(y_points)))
-#     max_polygon_y = int(np.ceil(max(y_points)))
-
-#     diff_x = max_polygon_x - min_polygon_x
-#     diff_y = max_polygon_y - min_polygon_y
-
-#     polygon_seed = f"{diff_x+(1<<32):b}" + f"{diff_y+(1<<32):b}"
-#     hashed_polygon_seed = int(hashlib.sha256(polygon_seed.encode()).hexdigest(), 16) % (2**32)
-#     np.random.seed(hashed_polygon_seed)
-
-#     count = 0
-#     color_list = []
-#     while count < 20:
-#         point = (np.random.randint(int(min(x_points)), int(max(x_points))), np.random.randint(int(min(y_points)), int(max(y_points))))
-#         point = (np.random.randint(int(min(x_points)), int(max(x_points))), np.random.randint(int(min(y_points)), int(max(y_points))))
-#         if pnpoly(len(x_points), x_points, y_points, point[0], point[1]) == 1:
-#             color_list.append(binary_image[point[1], point[0]])
-#             count += 1
-
-#     coverage_fraction = np.sum(color_list) / len(color_list)
-#     end_time = time.time()
-#     print("Time taken for checking polygon coverage: ", end_time - start_time)
-#     return coverage_fraction > threshold
